e1f0ca700de8b610aaf9c97e2dba7e34de4a0170
Peter Thiemann <peter.thiemann@gmail.com>
2022-02-26 23:21:05 +0100

    new files

---
+++ interp_Lexam.py
@@ -0,0 +1,33 @@
+from ast import *
+from interp_Lfun import InterpLfun
+from utils import *
+
+class InterpLexam(InterpLfun):
+
+  def interp_exp(self, e, env):
+    match e:
+      case List(es, Load()):
+        return [self.interp_exp(e, env) for e in es]
+      case BinOp(left, Mult(), right):
+          l = self.interp_exp(left, env); r = self.interp_exp(right, env)
+          return l * r
+      case BinOp(left, FloorDiv(), right):
+          l = self.interp_exp(left, env); r = self.interp_exp(right, env)
+          return l // r
+      case BinOp(left, Mult(), right):
+          l = self.interp_exp(left, env); r = self.interp_exp(right, env)
+          return l % r
+      case _:
+        return super().interp_exp(e, env)
+
+  def interp_stmts(self, ss, env):
+    if len(ss) == 0:
+      return
+    match ss[0]:
+      case Assign([Subscript(lst, index)], value):
+        lst = self.interp_exp(lst, env)
+        index = self.interp_exp(index, env)
+        lst[index] = self.interp_exp(value, env)
+        return self.interp_stmts(ss[1:], env)
+      case _:
+        return super().interp_stmts(ss, env)
