4b55c195ea13dbe9a7658ab0318f25c552e93cc9
Peter Thiemann <peter.thiemann@gmail.com>
2022-03-07 12:00:50 +0100

    fixing import

--- run-exam-tests.py
+++ run-exam-tests.py
@@ -2,7 +2,7 @@ from type_check_Lexam import TypeCheckLexam
 from interp_Lexam import InterpLexam
 from type_check_Cexam import TypeCheckCexam
 from interp_Cexam import InterpCexam
-from utils import run_tests, validate_tests
+from utils import enable_tracing, run_tests, validate_tests
 from compiler_Lexam import CompilerLexam
 import sys
 
