126ad0c79a3ff00fed00379f679a353e4a482b7b
Peter Thiemann <peter.thiemann@gmail.com>
2022-09-12 14:24:07 +0200

    tuple slicing must use constants

--- tests/exam-2/slicing2.py
+++ tests/exam-2/slicing2.py
@@ -3,6 +3,6 @@
 #golden=0
 i = input_int()
 j = input_int()
-t = (1,2,3,4,5,6,7,8,9,10)
+t = [1,2,3,4,5,6,7,8,9,10]
 s = t[i:j]
 print(len(s))
--- tests/exam-2/slicing3.py
+++ tests/exam-2/slicing3.py
@@ -3,7 +3,7 @@
 #golden=110
 i = input_int()
 j = input_int()
-t = (1,2,3,4,5,6,7,8,9,10)
+t = [1,2,3,4,5,6,7,8,9,10]
 s = t[i:j]
 print(len(s))
 print(s[0])
